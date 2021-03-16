47cfc2c01ecfc68e8e12461eb75866deafaae253
Ncode2014 <32153003+Ncode2014@users.noreply.github.com>
2021-03-16 19:22:31 +0700

    Update maker.sh

--- maker.sh
+++ maker.sh
@@ -69,22 +69,3 @@ sed -i '$!N; /^\(.*\)\n\1$/!P; D' op9.txt
 wget https://torrends.to/torrent-tracker-list/?download=latest --output-document=last.txt
 clear
 echo "grabbing shit has been dOne now shit now"
-
-# combine all text
-echo "combine now"
-find . -type f -name '*.txt' -exec cat {} + >> tracker1.txt
-echo "combine done now upload to github"
-echo "to doing that you must git status then using upload.sh"
-
-# check duplicate
-sort tracker1.txt | uniq | tee trackerX1.txt
-awk 1 ORS='\n\n' trackerX1.txt > trackerX.txt
-
-# checking space
-grep "\S" tracker1.txt > tracker12.txt
-awk 1 ORS='\n\n' tracker12.txt > trackerX_dup.txt
-
-#move
-mv trackerX_dup.txt Output
-mv trackerX.txt Output
-clear
